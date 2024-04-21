7fe3a19ea9a7560d04f25c7fa27ac6c49d6b7f0d
jcfebrer <jcfebrer@gmail.com>
2024-04-21 19:35:38 +0200

    FSLibraryCore conversión de objetos en bytes

--- FSLibraryCore/NumberUtils.cs
+++ FSLibraryCore/NumberUtils.cs
@@ -182,24 +182,6 @@ namespace FSLibraryCore
             return true;
         }
 
-
-        /// <summary>
-        /// Objects to byte array.
-        /// </summary>
-        /// <param name="obj">The object.</param>
-        /// <returns></returns>
-        public static byte[] ObjectToByteArray(object obj)
-        {
-            if (obj == null)
-                return null;
-            var bf = new BinaryFormatter();
-            using (var ms = new MemoryStream())
-            {
-                bf.Serialize(ms, obj);
-                return ms.ToArray();
-            }
-        }
-
         /// <summary>
         /// Reads all bytes.
         /// </summary>
@@ -657,6 +639,40 @@ namespace FSLibraryCore
         public static string GetNumbersAndLetters(string input)
         {
             return new string(input.Where(c => (char.IsDigit(c) || char.IsLetter(c))).ToArray());
+        }
+
+
+        /// <summary>
+        /// Convert an object to a Byte Array.
+        /// </summary>
+        public static byte[] ObjectToByteArray(object objData)
+        {
+            if (objData == null)
+                return default;
+
+            return Encoding.UTF8.GetBytes(JsonSerializer.Serialize(objData, GetJsonSerializerOptions()));
+        }
+
+        /// <summary>
+        /// Convert a byte array to an Object of T.
+        /// </summary>
+        public static T ByteArrayToObject<T>(byte[] byteArray)
+        {
+            if (byteArray == null || !byteArray.Any())
+                return default;
+
+            return JsonSerializer.Deserialize<T>(byteArray, GetJsonSerializerOptions());
+        }
+
+        private static JsonSerializerOptions GetJsonSerializerOptions()
+        {
+            return new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = null,
+                WriteIndented = true,
+                AllowTrailingCommas = true,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            };
         }
     }
 }
