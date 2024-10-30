1c4e7d8164a422b08ba08f12504a8c79dc61cee9
jcfebrer <jcfebrer@gmail.com>
2024-10-30 22:36:07 +0100

    Interface IParser

---
+++ FSParser/IParser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Specialized;
+
+namespace FSParser
+{
+    public interface IParser
+    {
+        NameValueCollection Memory { get; set; }
+
+        void Abort();
+        void Process(string code);
+        void Stop();
+        void ThreadReset();
+        void ThreadSet();
+        void ThreadWait();
+    }
+}
