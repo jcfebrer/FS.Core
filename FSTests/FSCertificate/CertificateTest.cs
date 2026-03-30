4f4afbe69904964f00936116264f1ce67ff84d50
jcfebrer <jcfebrer@gmail.com>
2026-03-30 12:31:08 +0200

    Test para certificados y Gemini

---
+++ FSTests/FSCertificate/CertificateTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using FSCertificate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FSTests.FSCertificate
+{
+    [TestClass]
+    public class CertificateTest
+    {
+        [TestMethod()]
+        public void TestGetCertificateByName()
+        {
+            X509Certificate2 cert = Certificate.GetCertificateByName("TestCertificate");
+
+            Assert.IsNotNull(cert);
+        }
+
+        [TestMethod()]
+        public void TestGetCertificateBySerialNumber()
+        {
+            X509Certificate2 cert = Certificate.GetCertificateBySerialNumber("50c96af9a925b0386061b5fdb6fd7215");
+
+            Assert.IsNull(cert);
+        }
+    }
+}
---
+++ FSTests/FSGoogle/GoogleGeminiTests.cs
@@ -0,0 +1,55 @@
+﻿#if NET8_0_OR_GREATER
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSTests.FSGoogle
+{
+    [TestClass()]
+    public class GeminiTests
+    {
+        IConfiguration config;
+        FSGoogleGemini.Library googleGemini;
+
+        public GeminiTests()
+        {
+            config = new ConfigurationBuilder()
+                            .AddJsonFile("appsettings.json")
+                            .Build();
+
+            googleGemini = new FSGoogleGemini.Library(config["GoogleGemini:ApiKey"]);
+        }
+
+        [TestMethod()]
+        public void TestListModelsAsync()
+        {
+            var response = googleGemini.ListModelsAsync();
+
+            Assert.IsNotNull(response);
+        }
+
+        [TestMethod()]
+        public void TestQuestion()
+        {
+            var response = googleGemini.Question("cual es la capital de checoslovaquia?");
+
+            Assert.IsNotNull(response);
+        }
+
+        [TestMethod()]
+        public void TestQuestionAsync()
+        {
+            var response = googleGemini.QuestionAsync("What is the capital of France?");
+
+            Assert.IsNotNull(response);
+        }
+    }
+}
+
+#endif
